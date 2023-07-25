bc277dc8190554007f308a26a644f6a85057d31b
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2023-07-25 10:49:29 -0300

    Updated subdomain enum and use of httpx to determine the protocol

--- khitai.sh
+++ khitai.sh
@@ -27,28 +27,23 @@ else
     bold=$(tput bold)
     echo;echo -e "\033[38;2;255;228;181m** Choose an option to begin the tests **\033[m";echo
     PS3=$'\n''-> '
-    options=("Security Headers" "HTTP Headers & Methods" "SSL Scan" "Check WAF" "Domain Spoofing" "Zone Transfer" "Wordpress Tests" "TCP Port Scan" "Slowloris DoS Test" "Quit")
+    options=("Security Headers" "HTTP Headers & Methods" "SSL Scan" "Check WAF" "Domain Spoofing" "Zone Transfer" "Wordpress Tests" "Subdomains" "TCP Port Scan" "Slowloris DoS Test" "Quit")
     select opt in "${options[@]}"
     do
         case $opt in
             "Security Headers")
-                clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Security Headers (HTTP)\033[m";echo
-                echo -e '\033[38;2;0;255;255mpython3 shcheck.py http://'$target'\033[m'
-                python3 shcheck.py http://$target
-                echo;echo "===========================================================================";echo;
-                echo -e "\033[38;2;220;20;60m${bold}>>> Security Headers (HTTPS)\033[m";echo
-                echo -e '\033[38;2;0;255;255mpython3 shcheck.py https://'$target'\033[m'
-                python3 shcheck.py https://$target
+                clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Security Headers\033[m";echo
+                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
+                echo -e '\033[38;2;0;255;255mpython3 shcheck.py '$proto'://'$target'\033[m'
+                python3 shcheck.py $proto://$target
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "HTTP Headers & Methods")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> HTTP Headers & Methods\033[m";echo
-                echo -e '\033[38;2;0;255;255mcurl -I http://'$target' -L -k -X OPTIONS -s --connect-timeout 15\033[m';echo
-                curl -I http://$target -L -k -X OPTIONS -s --connect-timeout 15
-		echo;echo "===========================================================================";echo;
-                echo -e '\033[38;2;0;255;255mcurl -I https://'$target' -L -k -X OPTIONS -s --connect-timeout 15\033[m';echo
-                curl -I https://$target -L -k -X OPTIONS -s --connect-timeout 15
+                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
+                echo -e '\033[38;2;0;255;255mcurl -I '$proto'://'$target' -L -k -X OPTIONS -s --connect-timeout 15\033[m';echo
+                curl -I $proto://$target -L -k -X OPTIONS -s --connect-timeout 15
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
@@ -61,67 +56,79 @@ else
                 ;;
             "Check WAF")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> WAF\033[m";echo
-                echo -e '\033[38;2;0;255;255mwafw00f -o - http://'$target'\033[m';echo
-                wafw00f -o - http://$target
-                echo;echo "===========================================================================";echo
-                echo -e '\033[38;2;0;255;255mwafw00f -o - https://'$target'\033[m';echo
-                wafw00f -o - https://$target
+                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
+                echo -e '\033[38;2;0;255;255mwafw00f -o - '$proto'://'$target'\033[m';echo
+                wafw00f -o - $proto://$target
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "Domain Spoofing")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Domain Spoofing\033[m";echo
-		echo -e "\033[38;2;255;228;181m-> SPF: \033[m";echo
-		echo -e '\033[38;2;0;255;255mhost -t txt '$target'\033[m';echo
-		host -t txt $target
-		echo;echo -e "\033[38;2;255;228;181m-> DMARC: \033[m";echo
-		echo -e '\033[38;2;0;255;255mhost -t txt _dmarc.'$target'\033[m';echo
-		host -t txt _dmarc.$target
-		echo;read -p $'\033[38;2;255;228;181m-> DKIM Selector (optional for DKIM check): \033[m' selector
-			if [[ ! -z $selector  ]]
-			then
-				echo;echo -e '\033[38;2;0;255;255mhost -t txt '$selector'._domainkey.'$target'\033[m';echo
-				host -t txt $selector._domainkey.$target
-			fi
-		echo;read -p $'\033[38;2;255;228;181m-> Send spoofing test - if yes, type the recipient | if no, just press ENTER: \033[m' recipient
-			if [[ ! -z $recipient  ]]
-			then
-				echo;echo -e '\033[38;2;0;255;255msendemail -f spoofed@'$target' -t '$recipient' -u "Spoofing Test" -m "Domain '$target' vulnerable to mail spoofing." -o tls=no\033[m'
-				echo;sendemail -f spoofed@$target -t $recipient -u "Spoofing Test" -m "Domain $target vulnerable to mail spoofing." -o tls=no
-				echo;echo -e "\033[38;2;255;228;181m-> Spoofing Mail Sent, check your inbox (if it goes to the spam folder, then it's not completely vulnerable).\033[m"
-                	fi
+				echo -e "\033[38;2;255;228;181m-> SPF: \033[m";echo
+				echo -e '\033[38;2;0;255;255mhost -t txt '$target'\033[m';echo
+				host -t txt $target
+				echo;echo -e "\033[38;2;255;228;181m-> DMARC: \033[m";echo
+				echo -e '\033[38;2;0;255;255mhost -t txt _dmarc.'$target'\033[m';echo
+				host -t txt _dmarc.$target
+				echo;read -p $'\033[38;2;255;228;181m-> DKIM Selector (optional for DKIM check): \033[m' selector
+				if [[ ! -z $selector  ]]
+				then
+					echo;echo -e '\033[38;2;0;255;255mhost -t txt '$selector'._domainkey.'$target'\033[m';echo
+					host -t txt $selector._domainkey.$target
+				fi
+				echo;read -p $'\033[38;2;255;228;181m-> Send spoofing test - if yes, type the recipient | if no, just press ENTER: \033[m' recipient
+				if [[ ! -z $recipient  ]]
+				then
+					echo;echo -e '\033[38;2;0;255;255msendemail -f spoofed@'$target' -t '$recipient' -u "Spoofing Test" -m "Domain '$target' vulnerable to mail spoofing." -o tls=no\033[m'
+					echo;sendemail -f spoofed@$target -t $recipient -u "Spoofing Test" -m "Domain $target vulnerable to mail spoofing." -o tls=no
+					echo;echo -e "\033[38;2;255;228;181m-> Spoofing Mail Sent, check your inbox (if it goes to the spam folder, then it's not completely vulnerable).\033[m"
+                fi
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "Zone Transfer")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Zone Transfer\033[m";echo
                 echo -e "\033[38;2;0;255;255mfor nserver in \$(host -t ns "$target" | cut -d ' ' -f4 | sed 's/.$//');do host -l -a "$target" \$nserver;done\033[m";echo
-		for nserver in $(host -t ns $target | cut -d ' ' -f4 | sed 's/.$//');do host -l -a $target $nserver;done
+				for nserver in $(host -t ns $target | cut -d ' ' -f4 | sed 's/.$//');do host -l -a $target $nserver;done
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "Wordpress Tests")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Wordpress XML-RPC\033[m";echo
-                echo -e "\033[38;2;0;255;255mcurl https://"$1"/xmlrpc.php -s --connect-timeout 15\033[m";echo
-				curl https://$1/xmlrpc.php -s --connect-timeout 15 > /tmp/xmlrpc-test.html
+                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
+                echo -e "\033[38;2;0;255;255mcurl "$proto"://"$target"/xmlrpc.php -s --connect-timeout 15\033[m";echo
+				curl $proto://$target/xmlrpc.php -s --connect-timeout 15 > /tmp/xmlrpc-test.html
 				isInFile=$(cat /tmp/xmlrpc-test.html | grep -c "XML-RPC")
 				if [ $isInFile -eq 0 ]; then
 					echo "XML-RPC not vulnerable."
 				else
    					echo "XML-RPC vulnerable!";echo
    					echo Response from the server: \"$(cat /tmp/xmlrpc-test.html)\";echo
-   					echo "URL: https://"$1"/xmlrpc.php"
+   					echo "URL: "$proto"://"$target"/xmlrpc.php"
 				fi
 				rm -rf /tmp/xmlrpc-test.html
                 echo;echo "===========================================================================";echo
                 echo -e "\033[38;2;220;20;60m${bold}>>> Wordpress WP-Cron\033[m";echo           
-				echo -e "\033[38;2;0;255;255mcurl -I https://"$1"/wp-cron.php -s --connect-timeout 15\033[m";echo
-				if [ $(curl -LI https://$1/wp-cron.php --connect-timeout 15 -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then
+				echo -e "\033[38;2;0;255;255mcurl -I "$proto"://"$target"/wp-cron.php -s --connect-timeout 15\033[m";echo
+				if [ $(curl -LI $proto://$target/wp-cron.php --connect-timeout 15 -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then
 					echo "WP-Cron vulnerable!";echo
-					curl -I https://$1/wp-cron.php -s --connect-timeout 15
-					echo "URL: https://"$1"/wp-cron.php"
+					curl -I $proto://$target/wp-cron.php -s --connect-timeout 15
+					echo "URL: "$proto"://"$target"/wp-cron.php"
 				else
 					echo "WP-Cron not vulnerable.";fi
+                echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
+                exec $0 $1
+                ;;
+            "Subdomains")
+                clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Subdomains\033[m";echo                
+				subfinder -silent -d $target >> subs.txt
+				query="SELECT ci.NAME_VALUE NAME_VALUE FROM certificate_identity ci WHERE ci.NAME_TYPE = 'dNSName' AND reverse(lower(ci.NAME_VALUE)) LIKE reverse(lower('%.$target'));"
+				(echo $target; echo $query | \
+    				psql -t -h crt.sh -p 5432 -U guest certwatch | \
+    				sed -e 's:^ *::g' -e 's:^*\.::g' -e '/^$/d' | \
+    				sed -e 's:*.::g';) | sort -u >> subs.txt
+				~/go/bin/httpx -silent -probe -list subs.txt | grep SUCCESS
+				rm subs.txt          
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
@@ -135,7 +142,7 @@ else
             "Slowloris DoS Test")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Slowloris DoS Test\033[m";echo
                 read -p $'\033[38;2;255;228;181m-> Target port: \033[m' port
-		echo;echo -e "\033[38;2;0;255;255mperl slowloris.pl -test -dns "$target" -port "$port"\033[m";echo
+				echo;echo -e "\033[38;2;0;255;255mperl slowloris.pl -test -dns "$target" -port "$port"\033[m";echo
                 perl slowloris.pl -test -dns $target -port $port
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
