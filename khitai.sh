42adf6cadfe7fd3d4bf7fe4782703ec8e114a83e
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2023-09-22 15:01:53 -0300

    Update khitai.sh

--- khitai.sh
+++ khitai.sh
@@ -14,20 +14,18 @@ echo -e "\033[38;2;220;20;60m
         \___]##.-----.##[___/        | . \  | | | | | | | |_  | (_| | | |
          |_|_|_|     |_|_|_|         |_|\_\ |_| |_| |_|  \__|  \__ _| |_|
          |_|_|_|_____|_|_|_|
-       #######################
+       #######################       https://github.com/rafaelbaldasso
 \033[m"
 
-if [ "$EUID" -ne 0 ]
-    then
-        echo;echo "[!] You must run the tool as root!";echo
-        exit
+if [ "$EUID" -ne 0 ]; then
+    echo;echo "[!] You must run the tool as root!";echo
+    exit
 fi
 
-if [ "$1" == "" ]
-then
-    echo;echo -e "\033[38;2;255;228;181m[>] Usage: $0 <domain>\033[m"
-    echo -e "\033[38;2;255;228;181m[>] Example: $0 github.com\033[m";echo
-    echo -e "\033[38;2;255;228;181m[!] Install dependencies by running the setup.sh as root!\033[m";echo
+if [[ "$1" == "" ]] || [[ "$1" != http?(s)://* ]]; then
+    echo;echo -e "\033[38;2;255;228;181m[>] Usage: $0 https://<domain>\033[m"
+    echo -e "\033[38;2;255;228;181m[>] Example: $0 https://github.com\033[m";echo
+    echo -e "\033[38;2;255;228;181m[!] If this is your first time using the tool, remember to install the dependencies by running the setup.sh as root!\033[m";echo
 else
     target=$1
     bold=$(tput bold)
@@ -39,17 +37,15 @@ else
         case $opt in
             "Security Headers")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Security Headers\033[m";echo
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
-                echo -e '\033[38;2;0;255;255mpython3 shcheck.py '$proto'://'$target'\033[m'
-                python3 shcheck.py $proto://$target
+                echo -e '\033[38;2;0;255;255mpython3 shcheck.py '$target'\033[m'
+                python3 shcheck.py $target
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "HTTP Headers & Methods")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> HTTP Headers & Methods\033[m";echo
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
-                echo -e '\033[38;2;0;255;255mcurl -I '$proto'://'$target' -L -k -X OPTIONS -s --connect-timeout 15\033[m';echo
-                curl -I $proto://$target -L -k -X OPTIONS -s --connect-timeout 15
+                echo -e '\033[38;2;0;255;255mcurl -I '$target' -L -k -X OPTIONS -s --connect-timeout 15\033[m';echo
+                curl -I $target -L -k -X OPTIONS -s --connect-timeout 15
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
@@ -62,18 +58,16 @@ else
                 ;;
             "Check WAF")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> WAF\033[m";echo
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
-                echo -e '\033[38;2;0;255;255mwafw00f -o - '$proto'://'$target'\033[m';echo
-                wafw00f -o - $proto://$target
+                echo -e '\033[38;2;0;255;255mwafw00f -o - '$target'\033[m';echo
+                wafw00f -o - $target
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
             "Clickjacking")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Clickjacking\033[m";echo
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
-                echo -e '\033[38;2;0;255;255mpython3 shcheck.py '$proto'://'$target' -d | egrep "X-Frame-Options|Content-Security-Policy"\033[m';echo
-                python3 shcheck.py $proto://$target -d | egrep "X-Frame-Options|Content-Security-Policy";echo
-                echo '<html><head><title>Clickjacking Test</title></head><body><h1>Clickjacking Test</h1><p><b>'$proto'://'$target'</b></p><iframe src="'$proto'://'$target'" width="800" height="500" margin-top="100" scrolling="yes" style="opacity:0.5"></iframe></body></html>' > /tmp/cj-test.html
+                echo -e '\033[38;2;0;255;255mpython3 shcheck.py '$target' -d | egrep "X-Frame-Options|Content-Security-Policy"\033[m';echo
+                python3 shcheck.py $target -d | egrep "X-Frame-Options|Content-Security-Policy";echo
+                echo '<html><head><title>Clickjacking Test</title></head><body><h1>Clickjacking Test</h1><p><b>'$target'</b></p><iframe src="'$target'" width="800" height="500" margin-top="100" scrolling="yes" style="opacity:0.5"></iframe></body></html>' > /tmp/cj-test.html
                 echo "Clickjacking Test HTML file (CTRL + CLICK to open):"
                 echo "file:///tmp/cj-test.html"
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
@@ -89,14 +83,12 @@ else
                 echo -e '\033[38;2;0;255;255mhost -t txt _dmarc.'$target'\033[m';echo
                 host -t txt _dmarc.$target
                 echo;read -p $'\033[38;2;255;228;181m-> DKIM Selector (optional for DKIM check): \033[m' selector
-                if [[ ! -z $selector  ]]
-                then
+                if [[ ! -z $selector  ]]; then
                 	echo;echo -e '\033[38;2;0;255;255mhost -t txt '$selector'._domainkey.'$target'\033[m';echo
                 	host -t txt $selector._domainkey.$target
                 fi
                 echo;read -p $'\033[38;2;255;228;181m-> Send spoofing test - if yes, type the recipient | if no, just press ENTER: \033[m' recipient
-                if [[ ! -z $recipient  ]]
-                then
+                if [[ ! -z $recipient  ]]; then
                 	echo;echo -e '\033[38;2;0;255;255msendemail -f spoofed@'$target' -t '$recipient' -u "Spoofing Test" -m "Domain '$target' vulnerable to mail spoofing." -o tls=no\033[m'
                 	echo;sendemail -f spoofed@$target -t $recipient -u "Spoofing Test" -m "Domain $target vulnerable to mail spoofing." -o tls=no
                 	echo;echo -e "\033[38;2;255;228;181m-> Spoofing Mail Sent, check your inbox (if it goes to the spam folder, then it's not completely vulnerable).\033[m"
@@ -113,27 +105,24 @@ else
                 ;;
             "Wordpress Tests")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Wordpress XML-RPC\033[m";echo
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
-                echo -e "\033[38;2;0;255;255mcurl "$proto"://"$target"/xmlrpc.php -s --connect-timeout 15\033[m";echo
-                curl $proto://$target/xmlrpc.php -s --connect-timeout 15 > /tmp/xmlrpc-test.html
+                echo -e "\033[38;2;0;255;255mcurl "$target"/xmlrpc.php -s --connect-timeout 15\033[m";echo
+                curl $target/xmlrpc.php -s --connect-timeout 15 > /tmp/xmlrpc-test.html
                 isInFile=$(cat /tmp/xmlrpc-test.html | grep -c "XML-RPC")
-                if [ $isInFile -eq 0 ]
-		        then
+                if [ $isInFile -eq 0 ]; then
                 	echo "XML-RPC not vulnerable."
                 else
                 	echo "XML-RPC vulnerable!";echo
                 	echo Response from the server: \"$(cat /tmp/xmlrpc-test.html)\";echo
-                	echo "URL: "$proto"://"$target"/xmlrpc.php"
+                	echo "URL: "$target"/xmlrpc.php"
                 fi
                 rm -rf /tmp/xmlrpc-test.html
                 echo;echo "===========================================================================";echo
                 echo -e "\033[38;2;220;20;60m${bold}>>> Wordpress WP-Cron\033[m";echo           
-                echo -e "\033[38;2;0;255;255mcurl -I "$proto"://"$target"/wp-cron.php -s --connect-timeout 15\033[m";echo
-                if [ $(curl -LI $proto://$target/wp-cron.php --connect-timeout 15 -o /dev/null -w '%{http_code}\n' -s) == "200" ]
-		        then
+                echo -e "\033[38;2;0;255;255mcurl -I "$target"/wp-cron.php -s --connect-timeout 15\033[m";echo
+                if [ $(curl -LI $target/wp-cron.php --connect-timeout 15 -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then
                 	echo "WP-Cron vulnerable!";echo
-                	curl -I $proto://$target/wp-cron.php -s --connect-timeout 15
-                	echo "URL: "$proto"://"$target"/wp-cron.php"
+                	curl -I $target/wp-cron.php -s --connect-timeout 15
+                	echo "URL: "$target"/wp-cron.php"
                 else
                 	echo "WP-Cron not vulnerable.";fi
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
@@ -154,38 +143,32 @@ else
                 ;;
             "Discovery")
                 clear;echo;echo -e "\033[38;2;220;20;60m${bold}>>> Discovery - Directories and Files (dirb)\033[m"
-                proto=$(~/go/bin/httpx -silent -u $target | cut -d ':' -f1)
                 wordlist="/usr/share/wordlists/dirb/common.txt"
                 delay=200
-                echo;echo -e "\033[38;2;0;255;255mCurrent target: "$proto"://"$target"/\033[m"
+                echo;echo -e "\033[38;2;0;255;255mCurrent target: "$target"/\033[m"
                 echo -e "\033[38;2;0;255;255mChange the discovery options below or press ENTER to ignore.\033[m"
                 echo;read -p $'\033[38;2;255;228;181m-> Change default target directory (i.e: /new/directory): \033[m' tgtdir
-                if [[ ! -z $tgtdir  ]]
-                then
+                if [[ ! -z $tgtdir  ]]; then
                 	target=$target$tgtdir
                 fi
                 echo;read -p $'\033[38;2;255;228;181m-> File extensions - disables the scan for directories (i.e: .txt,.cfg): \033[m' extens
-                if [[ ! -z $extens  ]]
-                then
+                if [[ ! -z $extens  ]]; then
                 	ext="-X "$extens
                 fi
                 echo;read -p $'\033[38;2;255;228;181m-> Delay (ms) - default 200: \033[m' newdelay
-                if [[ ! -z $newdelay  ]]
-                then
+                if [[ ! -z $newdelay  ]]; then
                 	delay=$newdelay
                 fi
                 echo;read -p $'\033[38;2;255;228;181m-> Custom wordlist - default /usr/share/wordlists/dirb/common.txt: \033[m' wlist
-                if [[ ! -z $wlist  ]]
-                then
+                if [[ ! -z $wlist  ]]; then
                 	wordlist=$wlist
                 fi
                 echo;read -p $'\033[38;2;255;228;181m-> Recursive - default enabled (d to disable): \033[m' recve
-                if [[ $recve == "d" ]] || [[ $recve == "D"  ]]
-                then
+                if [[ $recve == "d" ]] || [[ $recve == "D"  ]]; then
                 	recursive="-r"
                 fi
-                echo;echo -e "\033[38;2;0;255;255mdirb "$proto"://"$target" "$wordlist" -w "$recursive" -z "$delay" "$ext"\033[m"
-                dirb $proto://$target $wordlist -w $recursive -z $delay $ext	
+                echo;echo -e "\033[38;2;0;255;255mdirb "$target" "$wordlist" "$recursive" -z "$delay" "$ext"\033[m"
+                dirb $target $wordlist $recursive -z $delay $ext	
                 echo;read -p $'\033[38;2;255;215;0m< Press ENTER to continue >\033[m'
                 exec $0 $1
                 ;;
