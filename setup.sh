b8be93f00311d4f1956b63693874d0a4ecf0c2e3
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2024-02-26 11:07:09 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,14 +10,18 @@ echo;echo "-> Installing dependencies and starting services...";echo
 
 sudo apt install nmap dirb sslscan dnsutils sendemail postfix golang subfinder -y
 
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+git clone https://github.com/projectdiscovery/httpx.git; \
+cd httpx/cmd/httpx; \
+go build; \
+mv httpx /usr/local/bin/; \
+cd ../../..;
 
 sudo systemctl enable postfix && sudo service postfix start
 
 wget https://raw.githubusercontent.com/santoru/shcheck/master/shcheck.py
 wget https://raw.githubusercontent.com/Ogglas/Orignal-Slowloris-HTTP-DoS/master/slowloris.pl
 
-chmod +x shcheck.py slowloris.pl
+chmod +x shcheck.py slowloris.pl khitai.sh
 
 python3 -m pip install --upgrade requests
 python3 -m pip install ultimate_sitemap_parser
