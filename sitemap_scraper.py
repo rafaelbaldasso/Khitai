85aefd76a73b5dc1baa46061d5db80e26c669c1e
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2024-04-16 15:01:41 -0300

    Delete sitemap_scraper.py

--- sitemap_scraper.py
+++
@@ -1,8 +0,0 @@
-from usp.tree import sitemap_tree_for_homepage
-import sys
-
-# https://pypi.org/project/ultimate-sitemap-parser/
-
-tree = sitemap_tree_for_homepage(sys.argv[1])
-for page in tree.all_pages():
-    print(page)
